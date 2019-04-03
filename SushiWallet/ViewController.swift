2697dd8de2dd1b6d89a5fa6505b5533bec28835e
YutoMizutani <yuto.mizutani.dev@gmail.com>
2019-04-03 10:43:55 +0900

    :tada: Add project

---
+++ SushiWallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SushiWallet
+//
+//  Created by ym on 2019/04/03.
+//  Copyright © 2019 Yuto Mizutani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
